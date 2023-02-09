b1e1bc19b4c0ee6ddf45858b910acbaeac5552ca
Yoldaş Ulaş <yutalas@gmail.com>
2023-02-09 15:38:49 +0300

    Block Sliding Game
    Block Sliding Game Assets Added
    1 level is designed for block sliding game

--- Assets/Scripts/BlockSlidingGameSystem.cs
+++ Assets/Scripts/BlockSlidingGameSystem.cs
@@ -21,27 +21,44 @@ public class BlockSlidingGameSystem : MonoBehaviour
 
         columnCount = grids[0].row.Length;
 
-        // _characterTransform.position =
+        for (int j = 0; j < grids.Length ; j++) 
+        {
+            for (int i = 0; i < grids[0].row.Length; i++)
+            {
+                if (grids[j].row[i] == SlidingBlockMap.GridState.Start)
+                {
+                    currentPosition = new Vector2Int(i,j);
+                }
+            }
+        }
+            
+            // _characterTransform.position =
         //     new Vector2(Vector3.right * .5f * i * gridSize.x, Vector3.down * .5f * j * gridSize.y);
-
-
+        
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Debug.Log(currentPosition);
         
         if (GetGridState(currentPosition.x,currentPosition.y) == SlidingBlockMap.GridState.Goal)
             Debug.Log("Reached the goal!");
         
+        Debug.Log("horizontal input " + Input.GetAxis("Horizontal"));
         if (Input.GetAxis("Horizontal") < 0 && currentPosition.x > 0) 
         {
+            //Debug.Log("left input and current x is greater than 0");
             SlidingBlockMap.GridState veryLeftState = GetGridState(currentPosition.x -1,currentPosition.y);
 
+            Debug.Log("very left is " + veryLeftState);
             if (veryLeftState == SlidingBlockMap.GridState.Walkable)
-                SwapGrids(currentPosition,new Vector2Int (currentPosition.x -1,currentPosition.y));
-
+            {
+                SwapGrids(currentPosition, new Vector2Int(currentPosition.x - 1, currentPosition.y));
+                currentPosition.x--;
+            }
+                
             else if (veryLeftState == SlidingBlockMap.GridState.Block) 
             {
                 SlidingBlockMap.GridState verySecondLeft = GetGridState(currentPosition.x -2,currentPosition.y);
@@ -50,6 +67,7 @@ public class BlockSlidingGameSystem : MonoBehaviour
                 {
                     SwapGrids(new Vector2Int (currentPosition.x -2,currentPosition.y),new Vector2Int (currentPosition.x -1,currentPosition.y));
                     SwapGrids(currentPosition,new Vector2Int (currentPosition.x -1,currentPosition.y));
+                    currentPosition.x--;
                 }
             }
         }
@@ -59,7 +77,11 @@ public class BlockSlidingGameSystem : MonoBehaviour
             SlidingBlockMap.GridState veryRightState = GetGridState(currentPosition.x +1,currentPosition.y);
 
             if (veryRightState == SlidingBlockMap.GridState.Walkable)
+            {
+                currentPosition.x++;
                 SwapGrids(currentPosition,new Vector2Int (currentPosition.x +1,currentPosition.y));
+            }
+                
 
             else if (veryRightState == SlidingBlockMap.GridState.Block) 
             {
@@ -69,6 +91,7 @@ public class BlockSlidingGameSystem : MonoBehaviour
                 {
                     SwapGrids(new Vector2Int (currentPosition.x +2,currentPosition.y),new Vector2Int (currentPosition.x +1,currentPosition.y));
                     SwapGrids(currentPosition,new Vector2Int (currentPosition.x +1,currentPosition.y));
+                    currentPosition.x++;
                 }
             }
         }
@@ -78,7 +101,11 @@ public class BlockSlidingGameSystem : MonoBehaviour
             SlidingBlockMap.GridState veryUpState = GetGridState(currentPosition.x,currentPosition.y-1);
 
             if (veryUpState == SlidingBlockMap.GridState.Walkable)
+            {
                 SwapGrids(currentPosition,new Vector2Int (currentPosition.x,currentPosition.y-1));
+                currentPosition.y--;
+            }
+                
 
             else if (veryUpState == SlidingBlockMap.GridState.Block) 
             {
@@ -88,6 +115,7 @@ public class BlockSlidingGameSystem : MonoBehaviour
                 {
                     SwapGrids(new Vector2Int (currentPosition.x,currentPosition.y-2),new Vector2Int (currentPosition.x,currentPosition.y-1));
                     SwapGrids(currentPosition,new Vector2Int (currentPosition.x,currentPosition.y-1));
+                    currentPosition.y--;
                 }
             }
         }
@@ -97,7 +125,10 @@ public class BlockSlidingGameSystem : MonoBehaviour
             SlidingBlockMap.GridState veryDownState = GetGridState(currentPosition.x,currentPosition.y+1);
 
             if (veryDownState == SlidingBlockMap.GridState.Walkable)
-                SwapGrids(currentPosition,new Vector2Int (currentPosition.x,currentPosition.y+1));
+            {
+                SwapGrids(currentPosition, new Vector2Int(currentPosition.x, currentPosition.y + 1));
+                currentPosition.y++;
+            }
 
             else if (veryDownState == SlidingBlockMap.GridState.Block) 
             {
@@ -107,6 +138,7 @@ public class BlockSlidingGameSystem : MonoBehaviour
                 {
                     SwapGrids(new Vector2Int (currentPosition.x,currentPosition.y+2),new Vector2Int (currentPosition.x,currentPosition.y+1));
                     SwapGrids(currentPosition,new Vector2Int (currentPosition.x,currentPosition.y+1));
+                    currentPosition.y++;
                 }
             }
         }
